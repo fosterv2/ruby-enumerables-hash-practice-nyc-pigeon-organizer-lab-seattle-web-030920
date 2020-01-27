0a70ff7455263a3e1bd8d7f11a6a66b08955dbdb
Valerie Foster <fosterv2@outlook.com>
2020-01-27 00:22:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,6 +16,3 @@ def nyc_pigeon_organizer(data)
   end
   new_data
 end
-
-# pigeon_data = {:color => {:brown => ["Luca"], :black => ["Lola"],}, :gender => {:male => ["Luca"], :female => ["Lola"]},
-#                 :lives => {"Central Park" => ["Lola"], "Library" => ["Luca"]}}
